5b82718811748d9ffe181e9addc94d6d09d7a850
Dennis Jenkins <dennis.jenkins.75@gmail.com>
2022-12-29 17:39:58 -0800

    Fancy ANSI output...

--- format.sh
+++ format.sh
@@ -3,10 +3,20 @@
 MDFORMAT="${HOME}/.local/bin/mdformat"
 TYPOS="${HOME}/.cargo/bin/typos"
 
+GREEN="\x1b[32m"
+CLEAR="\x1b[0m"
+
+echo -e "${GREEN}clang-format${CLEAR}"
 clang-format -i $(
   find src -name "*.cc" -o -name "*.h" | sort
 )
 
-[[ -x ${MDFORMAT} ]] && ${MDFORMAT} README.md ./src ./doc
+[[ -x ${MDFORMAT} ]] && {
+  echo -e "${GREEN}mdformat${CLEAR}"
+  ${MDFORMAT} README.md ./src ./doc
+}
 
-[[ -x ${TYPOS} ]] && ${TYPOS} ./doc ./src
+[[ -x ${TYPOS} ]] && {
+  echo -e "${GREEN}typos${CLEAR}"
+  ${TYPOS} ./doc ./src
+}
