9a49555c20b0a775bfe7711bfef48ef1d83b3dbd
Dennis Jenkins <dennis.jenkins.75@gmail.com>
2023-01-19 17:39:47 -0800

    Another report: show all 'mesecons player killer' nodes.

---
+++ reports/list-all-playerkillers.sql
@@ -0,0 +1,6 @@
+select node_x, node_y, node_z, a.name
+from nodes l, actor a, node n
+where (l.node_id = n.node_id)
+  and (n.name = 'moremesecons_playerkiller:playerkiller')
+  and (l.owner_id = a.actor_id)
+order by a.name, l.pos_id;
