122b357d5cf08a11f6fc8cda6d57cf017f99f4f6
Dennis Jenkins <dennis.jenkins.75@gmail.com>
2023-02-24 18:43:32 -0800

    Removed unused code.  Cleaned up internal 'struct Bucket'.

--- src/lib/hashmap/hashmap.h
+++ src/lib/hashmap/hashmap.h
@@ -10,30 +10,24 @@
 
 template <typename TKey, typename TValue, class KeyHashFunc = std::hash<TKey>>
 class HashMap {
+private:
   // Value is prime, and determined experimentally (benchmarks).
   static constexpr size_t kHashBucketCount = 1117;
-
-  struct FakeBucket {
-    mutable std::mutex mutex_;
-    std::unordered_map<TKey, TValue, KeyHashFunc> data_;
-  };
 
   struct Bucket {
     mutable std::mutex mutex_;
     std::unordered_map<TKey, TValue, KeyHashFunc> data_;
-    char padding[128 - sizeof(FakeBucket)];
   };
 
-  struct BucketStats {
-    double load_factor;
-    size_t size;
+  struct BucketAligned : public Bucket {
+    char padding[128 - sizeof(Bucket)];
   };
 
 public:
   HashMap() : buckets_(kHashBucketCount) {
     // Want each 'Bucket' in its own CPU cache line to reduce cache evictions
     // due to adjacent mutex contention.  Values valid for g++ 11.3.1
-    check_size<Bucket, 128>();
+    check_size<BucketAligned, 128>();
   }
   ~HashMap() {}
 
@@ -41,20 +35,9 @@ public:
   // Returned value is not locked.
   TValue &Ref(const TKey &pos) {
     const size_t h = KeyHashFunc()(pos);
-    Bucket &bucket = buckets_.at(h % kHashBucketCount);
+    BucketAligned &bucket = buckets_.at(h % kHashBucketCount);
     std::unique_lock<std::mutex> lock(bucket.mutex_);
     return bucket.data_[pos];
-  }
-
-  std::vector<BucketStats> GetStats() const {
-    std::vector<BucketStats> stats;
-    std::transform(
-        buckets_.cbegin(), buckets_.cend(), std::back_inserter(stats),
-        [](const Bucket &bucket) -> BucketStats {
-          std::unique_lock<std::mutex> lock(bucket.mutex_);
-          return BucketStats{bucket.data_.load_factor(), bucket.data_.size()};
-        });
-    return stats;
   }
 
   size_t size() const {
@@ -66,7 +49,7 @@ public:
   }
 
 private:
-  std::vector<Bucket> buckets_;
+  std::vector<BucketAligned> buckets_;
 
   template <typename T, std::size_t expected_size,
             std::size_t actual_size = sizeof(T)>
